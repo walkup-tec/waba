b91b0ab9bed0cbce3f8befc8016639498f05851b
Marcelo Mozart <marcelo.mozart@icloud.com>
2026-09-08 15:46:20 -0300

    fix(laboratorio): evita 502 no Atualizar da Meta ao sincronizar templates

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-08-104500-tpl-sync-rate-limit";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-08-184000-tpl-sync-gateway-timeout";
