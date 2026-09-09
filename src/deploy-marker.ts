c10ccbfa9ba5c4ede3aca35858beac0ec9a2fcc1
Marcelo Mozart <marcelo.mozart@icloud.com>
2026-09-08 21:43:26 -0300

    chore: marker honor-opt-in-ptx-pause

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-003000-resume-paulo-teix";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-004500-honor-opt-in-ptx-pause";
