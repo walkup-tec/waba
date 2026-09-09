9cdb4fc832c50da0c03a222e3194316cc0df7543
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-09 20:32:10 -0300

    feat(campanhas): DEPLOY-2026-09-09-234800-OPT-IN-PTX-1000

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-232500-ANDRE-SEM-RIO";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-234800-OPT-IN-PTX-1000";
