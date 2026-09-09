2006cf5b7b29959b05a35347748664da5400c3cb
Marcelo Mozart <marcelo.mozart@icloud.com>
2026-09-08 21:55:59 -0300

    chore: marker opt-in-ptx-graph-send

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-004500-honor-opt-in-ptx-pause";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-010200-opt-in-ptx-graph-send";
