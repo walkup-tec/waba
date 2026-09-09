f7a11ba6b23fd02bd3fe33f63a2946f5e2352ffa
Marcelo Mozart <230033505+walkup-tec@users.noreply.github.com>
2026-09-09 10:10:22 -0300

    sync(master): dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-094000-opt-in-ptx-report-est";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-130200-template-multi-portfolio-waba";
