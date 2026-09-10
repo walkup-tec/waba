aba5152aa3e177f7e7284f3a1db4d0b9a97ed7dc
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-10 05:24:36 -0300

    fix(meta): posta template na WABA02 com o token da própria conta (dist/deploy-marker.js)

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-10-011800-GRAPH-COOLDOWN";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-10-081800-WABA02-TEMPLATE-TOKEN";
