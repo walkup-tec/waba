8b0e6d04c1949436a76e393d315fb9de1f024891
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-09 21:19:20 -0300

    fix(meta): dist/deploy-marker.js HEADER-UPLOAD-FAILOVER

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-09-241500-ANDRE-WABA02-REGISTER";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-10-001800-HEADER-UPLOAD-FAILOVER";
