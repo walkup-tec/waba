03d71837d8d7fabf5943921915c8654ba62957e4
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 08:10:23 -0300

    Marker TPL-SYNC-SISTER.

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-110800-TPL-SYNC-FAST";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-111800-TPL-SYNC-SISTER";
