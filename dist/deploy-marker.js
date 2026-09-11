4e1549500a386f44bc52296ddf65603595ddb9cc
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 09:19:00 -0300

    Portfolios Manager-only: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-113000-TPL-SKIP-UNMANAGED";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-122000-PORTFOLIO-MANAGER-ONLY";
