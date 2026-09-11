13037d39ab47d153ea9c4735137273123314c072
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 09:44:16 -0300

    Portfolios Marilza: src/deploy-marker.ts

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-125500-PORTFOLIO-KEEP-ADMIN-BM";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-130500-PORTFOLIO-MARILZA";
