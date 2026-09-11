f7ff0d1a7b5f5deec6ba7305d60863e57ec6cf06
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 08:02:18 -0300

    Build: marker TPL-SYNC-FAST.

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-105200-TPL-SYNC-TOKEN-WABAS";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-110800-TPL-SYNC-FAST";
