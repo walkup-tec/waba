7dcdd657440c454e600fe5eb3a872185a2e87c5d
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-11 13:54:07 -0300

    Transfer manual report: src/deploy-marker.ts

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-164800-DOUGLAS-MANUAL-REPORT";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-11-165200-TRANSFER-MANUAL-REPORT";
