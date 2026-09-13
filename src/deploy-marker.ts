fd8616f01d64670af10ef8d825a5b18b99b1d428
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-13 08:52:26 -0300

    Opt in PTX report override: src/deploy-marker.ts

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-13-114800-CONVITE-JANDIRA-REPORT";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-13-115200-OPTIN-PTX-REPORT";
