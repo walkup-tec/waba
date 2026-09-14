cd1d4627a0d19bf3e3becc9b8e4f6e7256154de9
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-14 10:33:06 -0300

    Mozart no min sends: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-124500-OFICIAL-IMAGE-1200X628";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-133000-MOZART-NO-MIN-SENDS";
