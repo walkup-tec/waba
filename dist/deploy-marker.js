5a46dcfac68d55eea916a0d9a1130454510e5525
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-14 08:25:49 -0300

    Cloud WABA01 visible: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-110200-CLOUD-WABA01-NUMBER";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-111800-CLOUD-WABA01-VISIBLE";
