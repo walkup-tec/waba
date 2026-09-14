981ca15e0a1a39d2250cc65fd04f8f806377fff6
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-14 09:18:07 -0300

    Cloud broadcast actions: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-114000-CLOUD-WABA01-CARD-NUMBERS";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-121500-CLOUD-BROADCAST-ACTIONS";
