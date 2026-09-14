e0470ab991bee022da6b9366254b52f7507c7e55
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-14 14:41:00 -0300

    Admin portfolios refresh: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-172800-FLAVIANE-PORTFOLIO";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-14-180500-ADMIN-PORTFOLIOS-REFRESH";
