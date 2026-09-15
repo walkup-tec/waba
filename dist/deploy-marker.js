d3a85da276e66b35a837345a06dead462cf7ebf5
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-15 08:02:14 -0300

    Leads PJ Lista: dist/deploy-marker.js

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-104500-LEADS-PJ-STOP";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-110500-LEADS-PJ-LISTA";
