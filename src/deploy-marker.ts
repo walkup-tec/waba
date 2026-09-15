480104e02ec5c0606cebe8448f0682ccb8febb3c
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-15 07:26:33 -0300

    Bump deploy marker to NATALLY-ID

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-102800-SELECT-FIRST";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-103500-NATALLY-ID";
