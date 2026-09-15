b3e3b399341e90471df1f97d3719f0684e5d980f
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-15 07:05:36 -0300

    Natally portfolio: src/deploy-marker.ts

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-100800-SELECT-PORTFOLIOS";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-101500-NATALLY-PORTFOLIO";
