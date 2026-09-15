4de7b8fc99fee2c1dd06a7a48d45f1055b5e4801
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-15 08:01:57 -0300

    Bump deploy marker to LEADS-PJ-LISTA

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-104500-LEADS-PJ-STOP";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-110500-LEADS-PJ-LISTA";
