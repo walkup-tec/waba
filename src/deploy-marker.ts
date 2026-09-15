b09907154ee09f909911b01497672ecf0d7d1a0c
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-15 16:36:48 -0300

    Templates: listar todos os portfólios e WABAs do Laboratório (src/deploy-marker.ts)

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-151000-RESTRITAS-HIDDEN";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-200000-TPL-PORTFOLIOS";
