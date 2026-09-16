6da6ee76872beba7b9fe2eaa41ffe8c977fd2204
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-16 07:44:45 -0300

    Conexão: remover Inscrever webhooks e Abrir atendimento

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-104500-VITORIA-FINALIZE";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-105000-CONEXAO-BTNS";
