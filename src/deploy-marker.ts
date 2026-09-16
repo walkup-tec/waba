ad3c0b24a71bc8e134cc6462c4cffa058fde3343
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-16 07:40:03 -0300

    Relatório: finalizar pontualmente VITORIA DA CONQUISTA

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-103000-VITORIA-REPORT";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-104500-VITORIA-FINALIZE";
