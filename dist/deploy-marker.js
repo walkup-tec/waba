061a3ca632c15d220884597f2adf4b8d2ca693dc
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-16 08:19:33 -0300

    Aviso de campanha: print do relatório do assinante e texto do WhatsApp

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-113000-TPL-SYNC-BM";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-115000-CAMP-DONE-COPY";
