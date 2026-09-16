b3e25c7f1eec45564de2b40b047783db33475a7c
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-16 07:28:04 -0300

    Relatório: override pontual de VITORIA DA CONQUISTA

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-15-200000-TPL-PORTFOLIOS";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-16-103000-VITORIA-REPORT";
