e763f4854a26450ed6e4e0baf22f1a28c2892b34
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-18 18:28:07 +0000

    Mantém Ver detalhes nas campanhas já finalizadas

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-18-175200-INBOX-CONTA-RESTRITA-V2";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-18-183000-CAMPANHA-DETALHES-FINALIZADA";
--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-18-175200-INBOX-CONTA-RESTRITA-V2";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-18-183000-CAMPANHA-DETALHES-FINALIZADA";
