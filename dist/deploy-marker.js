7369c736a50d130b42f28a13cb41642ec9dd6082
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-21 13:33:02 +0000

    fix(lab): retry GET de templates no 502 do Redeploy
    A lista do Disparo Cloud falhava na janela do Traefik. O GET passa a repetir sozinho e o texto deixa de culpar a Meta.

--- dist/deploy-marker.js
+++ dist/deploy-marker.js
@@ -2,4 +2,4 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WABA_DEPLOY_MARKER = void 0;
-exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-21-132200-DISPARO-CLOUD";
+exports.WABA_DEPLOY_MARKER = "DEPLOY-2026-09-21-133200-TPL-502-RETRY";
--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-21-132200-DISPARO-CLOUD";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-21-133200-TPL-502-RETRY";
