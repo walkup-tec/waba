056f242a265dc5730aab6698c58ab5b8519ce4a0
Walkup Sistemas <marcelo.mozart@icloud.com>
2026-09-23 22:02:47 +0000

    fix(laboratorio): restaura business_id no Hosted ES
    O deploy do card de Assinantes reverteu o index.html do Embedded Signup (voltaram response_type/redirect_uri e sumiu o business_id). Recoloca o BM do Laboratório na URL do onboard. Marker: DEPLOY-2026-09-23-221500-ES-ONBOARD-BM.

--- src/deploy-marker.ts
+++ src/deploy-marker.ts
@@ -1,3 +1,3 @@
 /** Validar em GET /health apos redeploy do servico waba_disparador (Easypanel). */
 
-export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-23-212000-assinantes-card-largo";
+export const WABA_DEPLOY_MARKER = "DEPLOY-2026-09-23-221500-ES-ONBOARD-BM";
